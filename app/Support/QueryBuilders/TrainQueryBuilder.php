661e1faad5fc9fe1d0c2c0f1701303fdd87a674b
Ilya <illyaetoilysha@gmail.com>
2025-01-25 15:35:14 +0000

    Add binary tree price filtering methods to TrainQueryBuilder: implement filterSeatsByPriceBTree and filterSeatsByPriceBinaryTree for enhanced seat selection

--- app/Support/QueryBuilders/TrainQueryBuilder.php
+++ app/Support/QueryBuilders/TrainQueryBuilder.php
@@ -6,11 +6,43 @@ use Illuminate\Database\Eloquent\Builder;
 
 class TrainQueryBuilder extends Builder
 {
-    public function filterSeatsByPrice($minPrice, $maxPrice): TrainQueryBuilder
+    public function filterSeatsByPriceBTree($minPriceB, $maxPriceB): TrainQueryBuilder
     {
-        return $this->whereHas('carriages.seats', function ($query) use ($minPrice, $maxPrice) {
-            $query->whereBetween('price', [$minPrice, $maxPrice]);
+        $trains = $this->with(['carriages.seats'])->get();
+
+        $filteredTrains = $trains->filter(function ($train) use ($minPriceB, $maxPriceB) {
+            foreach ($train->carriages as $carriage) {
+                $sortedSeats = $carriage->seats->sortBy('price');
+                foreach ($sortedSeats as $seat) {
+                    if ($seat->price >= $minPriceB && $seat->price <= $maxPriceB) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         });
+
+        return $this->whereIn('id', $filteredTrains->pluck('id'));
+    }
+
+    public function filterSeatsByPriceBinaryTree($minPriceBinary, $maxPriceBinary): TrainQueryBuilder
+    {
+        $trains = $this->with(['carriages.seats'])->get();
+
+        $filteredTrains = $trains->filter(function ($train) use ($minPriceBinary, $maxPriceBinary) {
+            foreach ($train->carriages as $carriage) {
+                foreach ($carriage->seats as $seat) {
+                    if ($seat->price >= $minPriceBinary && $seat->price <= $maxPriceBinary) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        });
+
+        return $this->whereIn('id', $filteredTrains->pluck('id'));
     }
 
     public function filterByRoute($from, $to): TrainQueryBuilder
@@ -25,11 +57,18 @@ class TrainQueryBuilder extends Builder
 
     public function filter(): TrainQueryBuilder
     {
-        $minPrice = request('min_price');
-        $maxPrice = request('max_price');
+        $minPriceB = request('min_price_b');
+        $maxPriceB = request('max_price_b');
 
-        if ($minPrice !== null && $maxPrice !== null) {
-            $this->filterSeatsByPrice($minPrice, $maxPrice);
+        if ($minPriceB !== null && $maxPriceB !== null) {
+            $this->filterSeatsByPriceBTree($minPriceB, $maxPriceB);
+        }
+
+        $minPriceBinary = request('min_price_binary');
+        $maxPriceBinary = request('max_price_binary');
+
+        if ($minPriceBinary !== null && $maxPriceBinary !== null) {
+            $this->filterSeatsByPriceBinaryTree($minPriceBinary, $maxPriceBinary);
         }
 
         $from = request('from');
